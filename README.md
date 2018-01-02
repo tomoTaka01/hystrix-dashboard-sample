d008596e7e64cde3180f3171061cf20fd40803e1
tomo taka <tomo_taka01@hotmail.com>
2018-01-02 13:04:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Spring Hystrix Dashboard
+  * as of 2018-01-02, SpringBoot version 2.0.0.M7
+  * to see the target project here: https://github.com/tomoTaka01/eureka-feign
+
+  ![GitHub Logo](/images/hystrix.png)
+
+  * start the Dashbored 
+```
+http://localhost:9001/hystrix
+```
+
+  * to monitor Stream
+```
+http://localhost:8888/sample/actuator/hystrix.streama
+```
+
---
+++ src/main/java/com/example/hystrixdashboardsample/HystrixDashboardSampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.hystrixdashboardsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardSampleApplication.class, args);
+	}
+}
